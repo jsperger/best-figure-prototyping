6909a87fae54041b83d372d7eb9d38a579feb80b
John Sperger <peaches@Johns-MBP-5.attlocal.net>
2024-05-01 14:14:38 -0400

    Rename subgroups to characteristics in forest plot

--- 05_plot_forest_subgroups.R
+++ 05_plot_forest_subgroups.R
@@ -1,17 +1,17 @@
 
 
 #Subgroup forest plot
-PlotSubgroupForest <- function(value_data, 
+PlotSubgroupForest <- function(value_data,
                                segments_data){
-  
-  
-  
+
+
+
   # Create the main plot without y-axis labels and ticks
   main_plot <- ggplot()+
     geom_segment(data = segments_data, aes(y = subgroup, yend = subgroup,
                                            x = personalized_treatments, xend = trial_average,
                                            color = color), size = 1) +
-    geom_point(data = value_data, aes(y = subgroup, x = value, 
+    geom_point(data = value_data, aes(y = subgroup, x = value,
                                 shape = method), size = 3)  +
     scale_color_manual(values = c("Positive" = "#E69F00", "Negative" = "#0072B2"), name = "Difference") +
     # geom_errorbarh(aes(xmin = CI_Lower, xmax = CI_Upper, height = 0.2)) +
@@ -20,21 +20,21 @@ PlotSubgroupForest <- function(value_data,
       axis.text.y=element_blank(),
       axis.ticks.y=element_blank()
     ) +
-    labs(x = "Negative Change in 24-week PEG", 
-         y = 'Subgroup') + 
+    labs(x = "Negative Change in 24-week PEG",
+         y = 'Patient Characteristic') +
     scale_shape_discrete(name = "Method")
 
-  
+
   # Create the label plot
   label_plot <- ggplot(value_data, aes(y = subgroup, x = 0)) +
     geom_text(aes(label = subgroup), hjust = 0) +
-    theme_void() + 
-    theme(plot.margin = margin(0,0,0,0)) + 
-    labs(y = 'Subgroup')
-  
+    theme_void() +
+    theme(plot.margin = margin(0,0,0,0)) +
+    labs(y = 'Patient Characteristic')
+
   # Combine the two plots
   combined_plot <- cowplot::plot_grid(label_plot, main_plot, rel_widths = c(1, 2), align = 'h')
-  
+
   # Print the combined plot
   return(combined_plot)
 }
